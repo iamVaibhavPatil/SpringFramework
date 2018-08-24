c80bfbffcbf40cf1e193cf82240deb0f0bb1bf44
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-08-24 17:55:42 -0400

    Retro Board
    Retro Board

---
+++ RetroBoard/src/main/java/com/retroboard/RetroBoardApplication.java
@@ -0,0 +1,12 @@
+package com.retroboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroBoardApplication.class, args);
+	}
+}
---
+++ RetroBoard/src/test/java/com/retroboard/RetroBoardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.retroboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RetroBoardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
